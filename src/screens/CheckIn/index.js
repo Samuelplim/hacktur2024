e1c81d93106f6ba227ede3358e7491378eb26210
Samuelplim <samuel12nerd@gmail.com>
2024-04-28 13:08:00 -0400

    alter: react-native0-maps to MapOpenLayer

--- src/screens/CheckIn/index.js
+++ src/screens/CheckIn/index.js
@@ -1,9 +1,10 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useRef } from "react";
 import { Dimensions, StyleSheet, Text, View } from "react-native";
-import MapView, { Marker } from "react-native-maps";
 import { ButtonPrimary } from "../../components/ButtonPrimary";
+import { MapOpenLayer } from "../../components/MapOpenLayer";
 
 export const CheckIn = () => {
+  const zoomToGeoJSONFuncRef = useRef();
   const [mapRegion, setMapRegion] = useState({
     latitude: 2.854127,
     longitude: -60.6455059,
@@ -21,15 +22,25 @@ export const CheckIn = () => {
     console.log("");
   }
 
+  const mapPressHandler = (coordinates) => {
+    Alert.alert(
+      "Map press",
+      `You pressed at position ${coordinates[0]}/${coordinates[1]}`
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.textTitle}>
         Você chegou no destino realize o check-In
       </Text>
+      <MapOpenLayer
+        onInitialized={(zoomToGeoJSON) =>
+          (zoomToGeoJSONFuncRef.current = zoomToGeoJSON)
+        }
+        onMapPress={mapPressHandler}
+      />
       <View style={styles.wrapper}>
-        <MapView style={styles.map} region={mapRegion}>
-          <Marker coordinate={youRegion} title="Você" />
-        </MapView>
         <ButtonPrimary onPress={addCheckIn} title={"Realizar Check-In"} />
       </View>
     </View>
